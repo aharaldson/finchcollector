dcacfc5bcaf3285d8da3173de408f29c9aa387bf
aharaldson <a.haraldson@gmail.com>
2023-12-19 17:59:00 -0700

    part 1 done, starts part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cars_index(request):
+    return render(request, 'cars/index.html', {
+        'cars': cars
+    })
+
+cars = [
+    {'make': 'Jeep', 'model': 'Grand Cherokee', 'year': 2018, 'kind': 'SUV'},
+    {'make': 'Porsche', 'model': 'Boxster', 'year': 2015, 'kind': 'sportscar'},
+]
